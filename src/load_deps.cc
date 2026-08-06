b9201307d291b4110c6559ec726aa514ae351208
Jurek Elliesen <91612967+aff3npirat@users.noreply.github.com>
2026-08-06 12:31:41 +0200

    Fix format

--- src/load_deps.cc
+++ src/load_deps.cc
@@ -91,9 +91,7 @@ void load_deps(fs::path const& repo, fs::path const& deps_root,
   }();
 
   auto const name = (fs::absolute(repo) / ".pkg.mutex").generic_string();
-  {
-    auto const create_file_if_not_exists = std::ofstream{name};
-  }
+  { auto const create_file_if_not_exists = std::ofstream{name}; }
   auto lock = boost::interprocess::file_lock{name.c_str()};
   if (!lock.try_lock()) {
     std::cout << "waiting for lock" << std::endl;
